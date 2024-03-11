31b3965298c12605aa184d58db5eaffc67050fe8
SHANTO323 <shantochando323@gmail.com>
2024-03-11 21:34:02 +0600

    new file

---
+++ codeForcesRound_642_B.cpp
@@ -0,0 +1,45 @@
+/*
+***** SHANTO
+***
+***** DATE :24/10/23
+*/
+//*******C++ HEADER*******//
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<array>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<utility>
+#include<unordered_set>
+#include<cstdlib>
+#include<iomanip>
+#include<numeric>
+using namespace std;
+//*******MACROS*******//
+#define ll			long long
+#define test(T)         	ll t;cin>>t;
+#define speedIO 		ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define setNum(n)		cout<<fixed<<setprecision(n);	
+#define endl    		"\n"
+#define elif    		else if
+#define pb      		push_back
+#define pop     		pop_back
+#define vbe(v)  		v.begin(),v.end()
+#define llv  			vector<ll>
+#define forA(v)			for(auto&i:v)
+#define forF(n0,n)		for(int i=n0;i<n;++i)
+//*******TEMPLATES *******//
+template<typename T>void out(T x){cout << x << endl;}
+
+//*************************************************************//
+ 
+int main() {
+	speedIO
+	test(t)
+	while(t--){
+		ll n;cin>>n;	
+	}
+}
